265865b0c617b74dac773dc6cc8e5b87f34ceeea
tattii <yuta.tatti@gmail.com>
2018-01-31 11:30:05 +0900

    upload S3

--- data/functions/sounding/main.py
+++ data/functions/sounding/main.py
@@ -1,92 +1,39 @@
-import re, json
-import datetime
-import requests
+import os
+import json, gzip
+import sounding
 
-endpoint = 'http://weather.uwyo.edu/cgi-bin/sounding?'
-
-def fetch_all():
-    year, month, dateh = last_datetime()
-
-    # JMA sounding points
-    # http://www.jma.go.jp/jma/kishou/know/upper/kaisetsu.html#kososite
-    ids = ['47401', '47412', '47418', '47582', '47600', '47646', '47678',
-        '47741', '47778', '47807', '47827', '47909', 
-        '47918', '47945', '47971', '47991']
-
-    data = {}
-    for id in ids:
-        data[id] = fetch_point(id, year, month, dateh)
+import boto3
+s3_client = boto3.client('s3')
+bucket = 'soundings'
 
 
-def last_datetime():
-    now = datetime.datetime.utcnow()
-    last = now - datetime.timedelta(hours=2)
-    year = str(last.year)
-    month = last.strftime("%m")
-    dateh = "{:0>2}{:0>2}".format(last.day, int(last.hour / 12) * 12)
-    return year, month, dateh
+def main():
+    data, time = sounding.fetch_all()
+
+    filename = time[:6] + '/sounding-' + time + '.json'
+    upload_json_gziped(data, filename)
+    upload_json_gziped(data, 'sounding-current.json')
 
 
-def fetch_point(point_id, year, month, dateh):
-    param = "YEAR={0}&MONTH={1}&FROM={2}&TO={2}&STNM={3}".format(year, month, dateh, point_id)
-    url = endpoint + "TYPE=TEXT%3ALIST&" + param
-    req = requests.get(url)
-    
 
-    # <H2>47778  Shionomisaki Observations at 12Z 26 Jan 2018</H2>
-    h2 = re.findall(r"<H2>(.*?)</H2>", req.text)
-    if len(h2) == 0:
-        print("cannot get: " + url)
-        return
-    name = h2[0].split(' ')[2]
-    print(url, h2[0])
+def upload_json_gziped(data, filename):
+    filename += '.gz'
+    tmpfile = '/tmp/' + os.path.basename(filename)
 
-    # <PRE>data</PRE>
-    pre = re.findall(r"<PRE>(.*?)</PRE>", req.text, re.DOTALL)
+    with gzip.open(tmpfile, 'w') as f:
+        f.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
 
-    # sounding
-    #-----------------------------------------------------------------------------
-    #   PRES   HGHT   TEMP   DWPT   RELH   MIXR   DRCT   SKNT   THTA   THTE   THTV
-    #   hPa     m      C      C      %    g/kg    deg   knot     K      K      K
-    #-----------------------------------------------------------------------------
-    rows = pre[0].splitlines()
+    print('upload: ' + filename)
+    s3_client.upload_file(tmpfile, bucket, filename, ExtraArgs={
+        'ContentType': 'application/json; charset=utf-8',
+        'ACL': 'public-read',
+        'ContentEncoding': 'gzip'})
 
-    levels = {}
-    for row in rows[5:]:
-        d = row.split()
-        pres = d[0]
-        data = d[1:]
 
-        if len(d) == 7: # no dew point
-            data = d[1:3] + [None, None, None] + d[3:6] + [None, d[6]]
-        elif len(d) == 5: # no wind
-            data = d[1:3] + [None, None, None, None, None, d[3], None, d[4]]
-        
-        levels[pres] = data
-
-    # Station information and sounding indices
-    # http://weather.uwyo.edu/upperair/indices.html
-    infos = pre[1].splitlines()
-    info = [row.split(": ")[1] for row in infos[1:]]
-    labels = [
-        'ID', 'TIME', 'SLAT', 'SLON', 'SELV',
-        'SHOW', 'LIFT', 'LFTV', 'SWEAT', 'KINX',
-        'CTOT', 'VTOT', 'TTOT',
-        'CAPE', 'CAPV', 'CINS', 'CINV',
-        'EQLV', 'EQTV', 'LFCT', 'LFCV', 'BRCH', 'BRCV',
-        'LCLT', 'LCLP', 'MLTH', 'MLMR',
-        'THTK', 'PWAT'
-    ]
-
-    indices = {}
-    for label, value in zip(labels, info):
-        indices[label] = value
-
-    return { 'name': name, 'indices': indices, 'levels': levels }
-
+# called by aws lambda
+def handler(event, context):
+    main()
 
 if __name__ == '__main__':
-    #fetch_point('47778')
-    fetch_all()
-
+    main()
 
---
+++ data/functions/sounding/sounding.py
@@ -0,0 +1,106 @@
+import re, json
+import datetime
+import requests
+
+endpoint = 'http://weather.uwyo.edu/cgi-bin/sounding?'
+
+
+def main():
+    data, time = fetch_all()
+
+    filename = "sounding-" + time + ".json"
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def fetch_all():
+    year, month, dayh = last_datetime()
+    time = year + month + dayh
+
+    # JMA sounding points
+    # http://www.jma.go.jp/jma/kishou/know/upper/kaisetsu.html#kososite
+    ids = ['47401', '47412', '47418', '47582', '47600', '47646', '47678',
+        '47741', '47778', '47807', '47827', '47909', 
+        '47918', '47945', '47971', '47991']
+
+    data = {}
+    for id in ids:
+        data[id] = fetch_point(id, year, month, dayh)
+
+    data['time'] = time
+
+    return data, time
+
+
+def last_datetime():
+    now = datetime.datetime.utcnow()
+    last = now - datetime.timedelta(hours=2)
+    year = str(last.year)
+    month = last.strftime("%m")
+    dayh = "{:0>2}{:0>2}".format(last.day, int(last.hour / 12) * 12)
+    return year, month, dayh
+
+
+def fetch_point(point_id, year, month, dayh):
+    param = "YEAR={0}&MONTH={1}&FROM={2}&TO={2}&STNM={3}".format(year, month, dayh, point_id)
+    url = endpoint + "TYPE=TEXT%3ALIST&" + param
+    req = requests.get(url)
+    
+
+    # <H2>47778  Shionomisaki Observations at 12Z 26 Jan 2018</H2>
+    h2 = re.findall(r"<H2>(.*?)</H2>", req.text)
+    if len(h2) == 0:
+        print("cannot get: " + url)
+        return
+    name = h2[0].split(' ')[2]
+    print(url, h2[0])
+
+    # <PRE>data</PRE>
+    pre = re.findall(r"<PRE>(.*?)</PRE>", req.text, re.DOTALL)
+
+    # sounding
+    #-----------------------------------------------------------------------------
+    #   PRES   HGHT   TEMP   DWPT   RELH   MIXR   DRCT   SKNT   THTA   THTE   THTV
+    #   hPa     m      C      C      %    g/kg    deg   knot     K      K      K
+    #-----------------------------------------------------------------------------
+    rows = pre[0].splitlines()
+
+    levels = {}
+    for row in rows[5:]:
+        d = row.split()
+        pres = d[0]
+        data = d[1:]
+
+        if len(d) == 7: # no dew point
+            data = d[1:3] + [None, None, None] + d[3:6] + [None, d[6]]
+        elif len(d) == 5: # no wind
+            data = d[1:3] + [None, None, None, None, None, d[3], None, d[4]]
+        
+        levels[pres] = data
+
+    # Station information and sounding indices
+    # http://weather.uwyo.edu/upperair/indices.html
+    infos = pre[1].splitlines()
+    info = [row.split(": ")[1] for row in infos[1:]]
+    labels = [
+        'ID', 'TIME', 'SLAT', 'SLON', 'SELV',
+        'SHOW', 'LIFT', 'LFTV', 'SWEAT', 'KINX',
+        'CTOT', 'VTOT', 'TTOT',
+        'CAPE', 'CAPV', 'CINS', 'CINV',
+        'EQLV', 'EQTV', 'LFCT', 'LFCV', 'BRCH', 'BRCV',
+        'LCLT', 'LCLP', 'MLTH', 'MLMR',
+        'THTK', 'PWAT'
+    ]
+
+    indices = {}
+    for label, value in zip(labels, info):
+        indices[label] = value
+
+    return { 'name': name, 'indices': indices, 'levels': levels }
+
+
+if __name__ == '__main__':
+    #fetch_point('47778')
+    main()
+
+
